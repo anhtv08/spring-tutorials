79c081c36421b6c782f50670d753bd07f3a084d3
anhtv08 <anhtv010888@gmail.com>
2018-06-11 20:03:35 +0800

    initial commits

---
+++ spring-vault-demo/src/main/java/com/example/springvaultdemo/SpringVaultDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.springvaultdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVaultDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVaultDemoApplication.class, args);
+	}
+}
