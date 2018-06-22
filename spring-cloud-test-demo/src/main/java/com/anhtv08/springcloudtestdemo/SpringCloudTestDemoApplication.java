6165a74a5f833b89c1e9e72bece48c2031f70e53
anhtv08 <anhtv010888@gmail.com>
2018-06-23 07:54:27 +0800

    adding spring cloud test demo

---
+++ spring-cloud-test-demo/src/main/java/com/anhtv08/springcloudtestdemo/SpringCloudTestDemoApplication.java
@@ -0,0 +1,12 @@
+package com.anhtv08.springcloudtestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudTestDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudTestDemoApplication.class, args);
+    }
+}
