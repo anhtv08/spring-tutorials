5ed1c32805af7eab35967758d673350750455fdb
anhtv08 <anhtv010888@gmail.com>
2018-07-18 21:16:40 +0800

    adding spring-cloud sleuth demo

--- spring-cloud-sleuth-demo/src/main/java/com/anhtv08/springcloudsleuthdemo/SpringCloudSleuthDemoApplication.java
+++ spring-cloud-sleuth-demo/src/main/java/com/anhtv08/springcloudsleuthdemo/SpringCloudSleuthDemoApplication.java
@@ -1,12 +1,12 @@
-package com.example.springvaultdemo;
+package com.anhtv08.springcloudsleuthdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringVaultDemoApplication {
+public class SpringCloudSleuthDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringVaultDemoApplication.class, args);
+        SpringApplication.run(SpringCloudSleuthDemoApplication.class, args);
     }
 }
--- spring-cloud-sleuth-demo/src/test/java/com/anhtv08/springcloudsleuthdemo/SpringCloudSleuthDemoApplicationTests.java
+++ spring-cloud-sleuth-demo/src/test/java/com/anhtv08/springcloudsleuthdemo/SpringCloudSleuthDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.springvaultdemo;
+package com.anhtv08.springcloudsleuthdemo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringVaultDemoApplicationTests {
+public class SpringCloudSleuthDemoApplicationTests {
 
     @Test
     public void contextLoads() {
